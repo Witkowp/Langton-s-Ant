9d8701efa208c705809601241e953cc6bcaf4e94
Maciej Ćmiel <maciej.cmiel.S@ee.pw.edu.pl>
2018-03-18 00:29:04 +0100

    read params from user / dynamic change of pic dims and noAnts

--- src/main.c
+++ src/main.c
@@ -5,8 +5,9 @@
 
 #define ROWS  50
 #define COLS  50
-#define GENS  50
 #define MSECS 25000
+static int size;
+static int noAnts;
 
 enum {
     WHITE, BLACK
@@ -23,14 +24,14 @@ typedef struct movement {
     int dir;
 } mm;
 
-static inline void step( mm *ants,int size) {
-    int i=0;
+static inline void step(mm *ants, int noAnts) {
+    int i = 0;
 
-    for(i;i<size;i++) {
+    for (i; i < noAnts; i++) {
         switch (world[ants[i].y][ants[i].x]) {
             case '1':
                 world[ants[i].y][ants[i].x] = '0';
-                print_world[ants[i].y][ants[i].x]='0';
+                print_world[ants[i].y][ants[i].x] = '0';
                 switch (ants[i].dir) {
                     case LEFT:
                         ants[i].y--;
@@ -52,7 +53,7 @@ static inline void step( mm *ants,int size) {
                 break;
             case '0':
                 world[ants[i].y][ants[i].x] = '1';
-                print_world[ants[i].y][ants[i].x]='1';
+                print_world[ants[i].y][ants[i].x] = '1';
                 switch (ants[i].dir) {
                     case LEFT:
                         ants[i].y++;
@@ -75,67 +76,75 @@ static inline void step( mm *ants,int size) {
     }
     int y = 0;
     int x;
-    for(y=0;y<size;y++){
-        print_world[ants[y].y][ants[y].x]='#';
+    for (y = 0; y < noAnts; y++) {
+        print_world[ants[y].y][ants[y].x] = '#';
     }
 
-    for(y=0;y<ROWS;y++){
+    for (y = 0; y < size; y++) {
         printf("\n");
-        for(x=0;x<COLS;x++){
-            printf("%c ",print_world[y][x]);
+        for (x = 0; x < size; x++) {
+            printf("%c ", print_world[y][x]);
         }
     }
-    /*for (y; y < ROWS; y++) {
-        //       _sleep(1000);
-        printf("\n");
-        int x = 0;
-        for (x; x < COLS; x++)
-            if (ants[i].y == y && ants[i].x == x)
-                printf("%c ", '@');
-            else
-                printf("%c ", (world[y][x]) ? '=' : '.');
-    }*/
-
 
 }
 
 int main(void) {
-    int size = 10;
-/*    mm cell = {
-            .x   = 20,
-            .y   = 20,
-            .dir = RIGHT,
-    };*/
+    printf("Podaj wymiary planszy: ");
+    scanf("%d", &size);
+    if (size < 1) {
+        printf("Zbyt mala plansza!\n");
+        return 100;
+    }
+    printf("\n");
+
+    int steps;
+    printf("Liczba krokow: ");
+    scanf("%d", &steps);
+    if (steps < 1) {
+        printf("Liczba krokow musi być dodatnia!\n");
+        return 101;
+    }
+    printf("\n");
+
+    printf("Liczba mrowek: ");
+    scanf("%d", &noAnts);
+    if (noAnts < 1) {
+         printf("Liczba mrowek musi być dodatnia!\n");
+         return 102;
+     }
+
+
     int rows;
     int columns;
-    for(rows=0;rows<ROWS;rows++){
-        for(columns=0;columns<COLS; columns++){
-            world[rows][columns]='1';
-            print_world[rows][columns]='1';
+    for (rows = 0; rows < size; rows++) {
+        for (columns = 0; columns < size; columns++) {
+            world[rows][columns] = '1';
+            print_world[rows][columns] = '1';
         }
     }
-    mm* array_of_ants = (mm*)malloc(size * sizeof(mm));
+    mm *array_of_ants = (mm *) malloc(noAnts * sizeof(mm));
     int x = 0;
-    for (x=0; x < size; x++) {
-        array_of_ants[x].x = 2 * x + size;
-        array_of_ants[x].y = x + size;
+    for (x = 0; x < noAnts; x++) {
+        array_of_ants[x].x = 2 * x + (size - 2 * noAnts) / 2;
+        array_of_ants[x].y = x + (size - noAnts) / 2;
         array_of_ants[x].dir = RIGHT;
     }
-    for (x=0; x < size; x++) {
-        printf("\n%i %i %i\n",array_of_ants[x].x,array_of_ants[x].y,array_of_ants[x].dir);
+    for (x = 0; x < noAnts; x++) {
+        printf("\n%i %i %i\n", array_of_ants[x].x, array_of_ants[x].y, array_of_ants[x].dir);
     }
     fflush(stdout);
     size_t i = 0;
 
-    for (i; i < GENS; i++) {
+    for (i; i < steps; i++) {
         if (usleep(MSECS) == -1) {
             fprintf(stderr, "usleep failed");
             exit(1);
         }
-        step(array_of_ants,size);
-        printf("Step: %d\n", i);
+        step(array_of_ants, noAnts);
+        printf("Step: %d\n", i + 1);
     }
-    for(x=0;x<size;x++){
+    for (x = 0; x < noAnts; x++) {
         free(array_of_ants);
     }
 
