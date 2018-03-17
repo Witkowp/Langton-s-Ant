6b9edade3aa0b0ca5c266a8ca618686a742a399f
Maciej Ćmiel <maciej.cmiel.S@ee.pw.edu.pl>
2018-03-17 10:55:38 +0100

    Add starter code (draws in console)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,95 @@
+
 #include <stdio.h>
-int main (void)
+#include <stdlib.h>
+#include <unistd.h>
+
+#define ROWS  50
+#define COLS  50
+#define GENS  150
+#define MSECS 25000
+
+enum { WHITE, BLACK };
+enum { LEFT, RIGHT, UP, DOWN };
+
+static char world[ROWS][COLS];
+
+typedef struct movement {
+    int  x;
+    int  y;
+    char dir;
+} mm;
+
+static inline void step(mm *cur)
 {
-   printf("Hello World!");
-   return 0;
+   switch (world[cur->y][cur->x]) {
+      case WHITE:
+         world[cur->y][cur->x] = BLACK;
+           switch (cur->dir) {
+              case LEFT:
+                 cur->y--;
+                   cur->dir = UP;
+                   break;
+              case RIGHT:
+                 cur->y++;
+                   cur->dir = DOWN;
+                   break;
+              case UP:
+                 cur->x++;
+                   cur->dir = RIGHT;
+                   break;
+              case DOWN:
+                 cur->x--;
+                   cur->dir = LEFT;
+                   break;
+           }
+           break;
+      case BLACK:
+         world[cur->y][cur->x] = WHITE;
+           switch (cur->dir) {
+              case LEFT:
+                 cur->y++;
+                   cur->dir = DOWN;
+                   break;
+              case RIGHT:
+                 cur->y--;
+                   cur->dir = UP;
+                   break;
+              case UP:
+                 cur->x--;
+                   cur->dir = LEFT;
+                   break;
+              case DOWN:
+                 cur->x++;
+                   cur->dir = RIGHT;
+           }
+   }
+
+   for (size_t y = 0; y < ROWS; y++) {
+      puts("");
+      for (size_t x = 0; x < COLS; x++)
+         if (cur->y == y && cur->x == x)
+            printf("%c ", '@');
+         else
+            printf("%c ", (world[y][x]) ? '=' : '.');
+   }
+}
+
+int main(void)
+{
+   mm cell = {
+           .x   = 20,
+           .y   = 20,
+           .dir = RIGHT,
+   };
+
+   for (size_t i = 0; i < GENS; i++) {
+      if (usleep(MSECS) == -1) {
+         fprintf(stderr, "usleep failed");
+         exit(1);
+      }
+      step(&cell);
+      printf("Step: %d\n", i);
+   }
+
+   puts("Done.");
 }
